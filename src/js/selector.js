0c14e202ed9bfc7d0e3a72f6367e85d45611fb50
inheeen <m.ayushina@yandex.ru>
2024-10-06 18:27:15 +0300

    2

---
+++ src/js/selector.js
@@ -0,0 +1,18 @@
+// Фильтрация элементов
+const filterBox = document.querySelectorAll('.items-box');
+
+document.querySelector('.btn-box').addEventListener('click', event => {
+    if (event.target.tagName !== 'BUTTON') return false;
+
+    let filterClass = event.target.dataset['f'];
+    console.log(filterClass);
+
+    filterBox.forEach(elem => {
+        if (!elem.classList.contains(filterClass)) {
+            elem.classList.add('hide');
+        } else {
+            elem.classList.remove('hide');
+        }
+    });
+});
+
